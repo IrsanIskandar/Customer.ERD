9b82ea6f06d6759b81ec34e6664ef02732f3df8c
Iskandar-dev <putra.iskandar@outlook.com>
2022-04-17 20:49:41 +0700

    fix EF Core

--- Customer.Data/CustomerContext.cs
+++ Customer.Data/CustomerContext.cs
@@ -10,7 +10,41 @@ namespace Customer.Data
 {
     public class CustomerContext : DbContext
     {
+        public DbSet<MelampauiBmpk> MelampauiBmpks { get; set; }
+        public DbSet<MelanggarBmpk> MelanggarBmpks { get; set; }
+        public DbSet<GolonganPihakKetiga> GolonganPihakKetigas { get; set; }
+        public DbSet<GoPublicMap> GoPublicMaps { get; set; }
+        public DbSet<GroupUsahaDebitur> GroupUsahaDebiturs { get; set; }
+        public DbSet<HubunganPelapor> HubunganPelapors { get; set; }
         public DbSet<MSAccountManager> MSAccountManagers { get; set; }
+        public DbSet<MSAddressType> MSAddressTypes { get; set; }
+        public DbSet<MSBentukBadanUsaha> MSBentukBadanUsahas { get; set; }
+        public DbSet<MSCustomerRelationship> MSCustomerRelationships { get; set; }
+        public DbSet<MSCustomerStatus> MSCustomerStatuses { get; set; }
+        public DbSet<MSExternalRating> MSExternalRatings { get; set; }
+        public DbSet<MSExternalRatingAgency> MSExternalRatingAgencies { get; set; }
+        public DbSet<MSNegara> MSNegaras { get; set; }
+        public DbSet<MSProvinsi> MSProvinsis { get; set; }
+        public DbSet<MSKota> MSKotas { get; set; }
+        public DbSet<MSKecamatan> MSKecamatans { get; set; }
+        public DbSet<MSKelurahan> MSKelurahans { get; set; }
+        public DbSet<MSKodepos> MSKodepos { get; set; }
+        public DbSet<MSOrgDirectorate> MSOrgDirectorates { get; set; }
+        public DbSet<MSOrgDivision> MSOrgDivisions { get; set; }
+        public DbSet<MSRatingModel> MSRatingModels { get; set; }
+        public DbSet<MSRiskRatingModel> MSRiskRatingModels { get; set; }
+        public DbSet<MSSector> MSSectors { get; set; }
+        public DbSet<MSSoeStatus> MSSoeStatuses { get; set; }
+        public DbSet<MSSubsector> MSSubsectors { get; set; }
+        public DbSet<SectorSubsector> SectorSubsectors { get; set; }
+        public DbSet<CustomerLog> SektorEkonomis { get; set; }
+        public DbSet<CustomerStatuses> CustomerStatuses { get; set; }
+        public DbSet<CustomerRatings> CustomerRatings { get; set; }
+        public DbSet<CustomerGroup> CustomerGroups { get; set; }
+        public DbSet<CustomerAccountManager> CustomerAccountManagers { get; set; }
+        public DbSet<CustomerContactPerson> CustomerContactPerson { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public DbSet<Customer.Domain.Models.Customer> Customers { get; set; }
 
         public CustomerContext()
         {
@@ -20,6 +54,7 @@ namespace Customer.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Data Source=NBPSS068\\SQLSVRDEV89;Database=CustomerModel;User Id=sa;Password=intikom123;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //optionsBuilder.UseNpgsql("Host=localhost;Database=CustomerModel;Username=postgres;Password=irs123*");
             optionsBuilder.UseNpgsql("Host=localhost;Database=CustomerModel;Username=postgres;Password=123456");
         }
 
